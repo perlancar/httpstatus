4d09314f461569239ade703b70088afe2916cacc
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2019-12-24 17:04:12 +0700

    dzilify

---
+++ lib/App/httpstatus/more.pm
@@ -0,0 +1,37 @@
+package App::httpstatus::more;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Display HTTP status code information
+
+=head1 SYNOPSIS
+
+See L<httpstatus> script.
+
+
+=head1 DESCRIPTION
+
+This is a fork of L<App::httpstatus> 2.0.0 to add more codes. The original
+script has not been updated since 2013.
+
+
+=head1 SEE ALSO
+
+L<App::httpstatus>, the original script.
+
+
+=head1 ORIGINAL AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+
+=head1 ORIGINAL LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
--- t/00_compile.t
+++
@@ -1,8 +0,0 @@
-use strict;
-use Test::More;
-
-use_ok $_ for qw(
-    App::httpstatus
-);
-
-done_testing;
